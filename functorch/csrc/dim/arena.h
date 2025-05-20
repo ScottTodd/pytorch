d04fad93f133bf4e84d2dee9c39d0ddb1f505b78
Scott Todd <scott.todd0@gmail.com>
2025-05-20 14:29:25 -0700

    [Windows] Rework defines for clang-cl compatibility.
    Co-authored-by: Aaryaman Vasishta <jem456.vasishta@gmail.com>
    Co-authored-by: Scott Todd <scott.todd0@gmail.com>

--- functorch/csrc/dim/arena.h
+++ functorch/csrc/dim/arena.h
@@ -8,7 +8,7 @@
 #include <ATen/ATen.h>
 #include "minpybind.h"
 
-#ifdef _WIN32
+#if defined(_WIN32) && !(defined(__clang__) && defined(_MSC_VER))
 #include <intrin.h>
 // https://stackoverflow.com/questions/355967/how-to-use-msvc-intrinsics-to-get-the-equivalent-of-this-gcc-code
 inline unsigned int __builtin_clz(unsigned int x) {
